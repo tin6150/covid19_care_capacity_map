8e94779e06ab78645046cf27112a58038f284197
tin6150 <sn+newbox@grumpyxmas.com>
2020-05-03 23:15:29 -0700

    this one git commit the generated gson.  wget --quiet on fetch sh script

--- fetchNewCaseDataFile.sh
+++ fetchNewCaseDataFile.sh
@@ -7,7 +7,7 @@
 # this worked ok in USS, need some ndjson tool from npm install
 
 #wget https://covidtracking.com/api/v1/states/daily.json   -O cases_daily_dw_2020_0419.json    # one version was checked into git
-wget https://covidtracking.com/api/v1/states/daily.json   -O cases_daily_automaton.json 
+wget --quiet https://covidtracking.com/api/v1/states/daily.json   -O cases_daily_automaton.json 
 
 
 # if all ok, then can invoke AlgC_state+capa+case+calc.sh
